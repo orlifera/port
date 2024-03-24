c6e0050b58ff83e9bf994f3f3ed594750d5ae351
unknown <oferazzani125@gmail.com>
2024-03-24 20:53:32 +0100

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {
-  createHashRouter,
-  RouterProvider
-} from 'react-router-dom';
-import App from './App';
-import './index.css';
-
-const router = createHashRouter([
-  {
-    path: "/*",
-    element: <App />,
-  }
-]);
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={ router } />
-  </React.StrictMode>
-);
+  <Router>
+    <App />
+  </Router>,
+)
