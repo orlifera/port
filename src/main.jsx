b50d8ad499e5596ac463baa203624b24b230a8a3
unknown <oferazzani125@gmail.com>
2024-03-24 21:52:49 +0100

    test

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,8 @@ import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouterRouter>
+    <BrowserRouter>
       <App />
-    </BrowserRouterRouter>
+    </BrowserRouter>
   </React.StrictMode>
 )
