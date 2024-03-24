5106456211c31a1aea189e0be523dec63ebdb943
unknown <oferazzani125@gmail.com>
2024-03-24 20:36:50 +0100

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { HashRouter as Router } from 'react-router-dom'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {
+  createHashRouter,
+  RouterProvider
+} from 'react-router-dom';
+import App from './App';
+import './index.css';
+
+const router = createHashRouter([
+  {
+    path: "/*",
+    element: <App />,
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Router>
-    <App />
-  </Router>,
-)
+  <React.StrictMode>
+    <RouterProvider router={ router } />
+  </React.StrictMode>
+);
