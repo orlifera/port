940dccfffdeb86abe5f6fa55d74dfa66e7efcbc5
unknown <oferazzani125@gmail.com>
2024-04-24 14:18:53 +0200

    hotfix??

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import Switch from './Switch'
 import MobileNav from './MobileNav';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 function Navbar() {
     const [size, setSize] = useState(window.innerWidth);
@@ -23,16 +23,16 @@ function Navbar() {
                 </a>
                 <ul>
                     <li>
-                        <Link activeClassName="active" to="/"><span lang='en'>Home</span></Link>
+                        <NavLink activeClassName="active" to="/"><span lang='en'>Home</span></NavLink>
                     </li>
                     <li>
-                        <Link activeClassName="active" to="/About"><span lang='en'>About Me</span></Link>
+                        <NavLink activeClassName="active" to="/About"><span lang='en'>About Me</span></NavLink>
                     </li>
                     <li>
-                        <Link activeClassName="active" to="/Projects">Progetti</Link>
+                        <NavLink activeClassName="active" to="/Projects">Progetti</NavLink>
                     </li>
                     <li>
-                        <Link activeClassName="active" to="/Contact">Contatti</Link>
+                        <NavLink activeClassName="active" to="/Contact">Contatti</NavLink>
                     </li>
                     <li className="switch">
                         <Switch />
